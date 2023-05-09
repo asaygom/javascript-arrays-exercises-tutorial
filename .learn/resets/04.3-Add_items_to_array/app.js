23122e488729028f1f458ead350c25a57cc5478d
Alexis Sayago <102667039+asaygom@users.noreply.github.com>
2023-05-09 19:43:26 +0000

    some solutions

--- .learn/resets/04.3-Add_items_to_array/app.js
+++ .learn/resets/04.3-Add_items_to_array/app.js
@@ -1,7 +1,5 @@
 let arr = [4,5,734,43,45];
 
 // Your code here, use the push function and the random function.
-let x=Math.floor(Math.random()*100);
-arr.push(x);
-arr.push(x);
+
 console.log(arr);
--- .learn/resets/07.2-Letter-Counter/app.js
+++ .learn/resets/07.2-Letter-Counter/app.js
@@ -1,11 +1,6 @@
 let par = "Lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur eget bibendum turpis Curabitur scelerisque eros ultricies venenatis mi at tempor nisl Integer tincidunt accumsan cursus"
 let counts = {};
-let letter = "";
 
 // your code here
-for (let i=0;i<par.length;i++){
-    if (par.charAt(i)!=" "){letter = par.charAt(i)}
-    counts.letter += 1;
-}
 
 console.log(counts);
