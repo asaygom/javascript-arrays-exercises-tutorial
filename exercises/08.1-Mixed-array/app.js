21d6b91a864997ac66d2cf25c45fe11c9ed5c9c6
Alexis Sayago <102667039+asaygom@users.noreply.github.com>
2023-05-09 21:13:35 +0000

    until 10

--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,3 +1,12 @@
 let mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
-
 //your code here
+let newArr=[];
+
+function typeofMyArr(array){
+    for (let i=0;i<array.length;i++){
+        newArr.push(typeof(array[i]));
+    }
+    console.log(newArr);
+}
+
+typeofMyArr(mix);
--- exercises/08.3-Sum-all-items/app.js
+++ exercises/08.3-Sum-all-items/app.js
@@ -1,6 +1,11 @@
 function sumTheElements(theArray){
 	let total = 0;
 	//your code here
-
+	for (let i=0;i<theArray.length;i++){
+		total = total + theArray[i];
+	}
 	return total;
 }
+
+let myArr=[2,13,34,5];
+console.log(sumTheElements(myArr));
